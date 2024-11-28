a761a7ffca8e203fa5d49432281adb1c3c2d5529
Hashir Khan <hashirahmedkhan123@gmail.com>
2024-11-29 00:45:56 +0500

    aaa

--- src/lib/reviewService.js
+++ src/lib/reviewService.js
@@ -1,12 +1,12 @@
 // src/lib/reviewService.js
-
+import { query } from './db';  
 export const addReview = async (reviewData) => {
     const { contract_id, student_id, teacher_id, rating, review_text } = reviewData;
-    const query = `
+    const text = `
         INSERT INTO reviews (contract_id, student_id, teacher_id, rating, review_text)
         VALUES ($1, $2, $3, $4, $5) RETURNING *
     `;
     const values = [contract_id, student_id, teacher_id, rating, review_text];
-    const { rows } = await db.query(query, values);
+    const { rows } = await query(text, values);
     return rows[0];
 };
