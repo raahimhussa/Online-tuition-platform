d317751185945fdd963cae93445cf039b70b6d2d
Aawaiz <aawaizali8@gmail.com>
2024-12-06 04:48:12 +0500

    reviews-api

--- src/app/api/reviews/route.js
+++
@@ -1,9 +0,0 @@
-// src/app/api/reviews/route.js
-
-import { addReview } from '../../../lib/reviewService';
-
-export async function POST(req) {
-    const data = await req.json();
-    const review = await addReview(data);
-    return new Response(JSON.stringify(review), { status: 201 });
-}
