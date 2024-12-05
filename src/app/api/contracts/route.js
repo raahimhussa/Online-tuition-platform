869cc340fec975a7a5336ffbcd30d107095bab9f
Hashir Khan <hashirahmedkhan123@gmail.com>
2024-12-05 16:50:01 +0500

    contract-api

--- src/app/api/contracts/route.js
+++ src/app/api/contracts/route.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { createContract, getContractById, updateContractStatus, deleteContract ,getAllContracts} from '../../../lib/contractService';
-=======
-import { createContract, getContractById, updateContractStatus, deleteContract,getAllContracts } from '../../../lib/contractService';
->>>>>>> a037386eaf962f711f7057bc8d237fc7d78aba1d
 
 export async function POST(req) {
   const client = await query.connect(); // Start a client connection for transaction control
@@ -51,7 +47,6 @@ export async function POST(req) {
 export async function GET(req) {
     try {
       const url = new URL(req.url);
-<<<<<<< HEAD
       const contractId = url.searchParams.get('contract_id'); // Get contract_id from query parameters
   
       if (contractId) {
@@ -64,24 +59,11 @@ export async function GET(req) {
               headers: { 'Content-Type': 'application/json' },
             }
           );
-=======
-      const contractId = url.searchParams.get('contract_id'); // Get `contract_id` from query parameters
-  
-      if (contractId) {
-        // Fetch contract by ID
-        const contract = await getContractById(contractId);
-        if (!contract) {
-          return new Response(JSON.stringify({ message: 'Contract not found' }), {
-            status: 404,
-            headers: { 'Content-Type': 'application/json' },
-          });
->>>>>>> a037386eaf962f711f7057bc8d237fc7d78aba1d
         }
         return new Response(JSON.stringify(contract), {
           status: 200,
           headers: { 'Content-Type': 'application/json' },
         });
-<<<<<<< HEAD
       }
   
       const contracts = await getAllContracts();
@@ -101,22 +83,6 @@ export async function GET(req) {
           headers: { 'Content-Type': 'application/json' },
         }
       );
-=======
-      } else {
-        // Fetch all contracts if no ID is provided
-        const contracts = await getAllContracts();
-        return new Response(JSON.stringify(contracts), {
-          status: 200,
-          headers: { 'Content-Type': 'application/json' },
-        });
-      }
-    } catch (error) {
-      console.error('Error fetching contract(s):', error);
-      return new Response(JSON.stringify({ message: 'Failed to fetch contracts', error: error.message }), {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      });
->>>>>>> a037386eaf962f711f7057bc8d237fc7d78aba1d
     }
   }
 
