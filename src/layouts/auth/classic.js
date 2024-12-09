3a7041e356d078d238226aa0cfed731d16a7541b
Syed Ali Murtaza <alimurtazaathar@gmail.com>
2024-12-09 09:16:56 +0500

    updates

--- src/layouts/auth/classic.js
+++ src/layouts/auth/classic.js
@@ -83,7 +83,7 @@ export default function AuthClassicLayout({ children, image, title }) {
       <Box
         component="img"
         alt="auth"
-        src={image || '/assets/illustrations/illustration_dashboard.png'}
+        src={image || '/assets/illustrations/login_page.png'}
         sx={{ maxWidth: 720 }}
       />
 
